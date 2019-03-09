80c6cc661b134072b062b424bbef808c0d2a45c8
Kyle Burger <kyle.W.burger@ge.com>
2019-03-09 16:26:49 -0600

    Star wars api test data

--- js/app.js
+++ js/app.js
@@ -13,24 +13,19 @@ var app = angular.module('myApp', []);
 app.controller('myCtrl', function ($scope, $http) {
     $scope.firstName = "John";
     $scope.lastName = "Doe";
-    $scope.starWarsAPInames = [];
     // Simple GET request example:
     $http({
         method: 'GET',
-        //url: 'https://jsonplaceholder.typicode.com/todos/1'
         url: 'https://swapi.co/api/people/'
     }).then(function successCallback(response) {
-            console.log(response.data.results);
-            for (var i = 0; i < response.data.results.length; i++) {
-                console.log(response.data.results[i].name);
-                $scope.starWarsAPInames.push(response.data.results[i].name);
-            }
-            //console.log("This works!")
-            $scope.databaseResponse = response.data.results;
+            //console.log(response.data.results.length);
+            //for (var i = 0; i < response.data.results.length; i++) {
+            //}
             // when the response is available
+            $scope.databaseResponse = response.data.results;
         },
         function errorCallback(response) {
-            console.log("this does not work.");
+            //console.log("this does not work.");
             $scope.databaseResponse = response;
         });
 
