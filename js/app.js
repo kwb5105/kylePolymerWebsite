daeb2bd09cd597883058e7a2a1c97fb1e71ecbe1
Kyle Burger <kyle.W.burger@ge.com>
2019-03-09 15:09:39 -0600

    Using star wars api to test front end angular functionality

--- js/app.js
+++ js/app.js
@@ -13,19 +13,20 @@ var app = angular.module('myApp', []);
 app.controller('myCtrl', function ($scope, $http) {
     $scope.firstName = "John";
     $scope.lastName = "Doe";
-
+    $scope.starWarsAPInames = [];
     // Simple GET request example:
     $http({
         method: 'GET',
-        url: 'https://jsonplaceholder.typicode.com/todos/1'
+        //url: 'https://jsonplaceholder.typicode.com/todos/1'
+        url: 'https://swapi.co/api/people/'
     }).then(function successCallback(response) {
-            console.log(response.data);
-            for (var i = 0; i < response.data.length; i++) {
-                console.log(response.data);
-                console.log(response.data[i]);
+            console.log(response.data.results);
+            for (var i = 0; i < response.data.results.length; i++) {
+                console.log(response.data.results[i].name);
+                $scope.starWarsAPInames.push(response.data.results[i].name);
             }
             //console.log("This works!")
-            $scope.databaseResponse = response;
+            $scope.databaseResponse = response.data.results;
             // when the response is available
         },
         function errorCallback(response) {
