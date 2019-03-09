0c9ac80bc8e441b061b914cba41d27bd7f509a97
Kyle Burger <kyle.W.burger@ge.com>
2019-03-09 14:27:16 -0600

    Using app controller and angular js http function

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-var app = angular.module("myApp", []);
+/*var app = angular.module("myApp", []);
 app.directive("w3TestDirective", function () {
 
     return {
@@ -6,5 +6,31 @@ app.directive("w3TestDirective", function () {
         template: "<h1>Directing Testing from Javascript file </h1>"
 
     };
+
+});
+*/
+var app = angular.module('myApp', []);
+app.controller('myCtrl', function ($scope, $http) {
+    $scope.firstName = "John";
+    $scope.lastName = "Doe";
+
+    // Simple GET request example:
+    $http({
+        method: 'GET',
+        url: 'https://jsonplaceholder.typicode.com/todos/1'
+    }).then(function successCallback(response) {
+            console.log(response.data);
+            for (var i = 0; i < response.data.length; i++) {
+                console.log(response.data);
+                console.log(response.data[i]);
+            }
+            //console.log("This works!")
+            $scope.databaseResponse = response;
+            // when the response is available
+        },
+        function errorCallback(response) {
+            console.log("this does not work.");
+            $scope.databaseResponse = response;
+        });
 
 });
